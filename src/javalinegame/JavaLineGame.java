568e4a3b7d63cffd91368df6f115ae425abeb321
Jerimiah Gazlay <laconicbill3169@yahoo.com>
2012-02-22 12:46:47 -0500

    I have added background and a box to catch the javelins.

--- src/javalinegame/JavaLineGame.java
+++ src/javalinegame/JavaLineGame.java
@@ -7,8 +7,7 @@ import java.awt.event.*;
 
 
 /**
- * @author Jerimiah Gazlay
- * Page 
+ * @author Jerimiah Gazla
  */
 public class JavaLineGame extends JFrame 
 {
@@ -31,6 +30,13 @@ public class JavaLineGame extends JFrame
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         
+        Move1 m1 = new Move1();
+        Move2 m2 = new Move2();
+        Move3 m3 = new Move3();
+        m1.start();
+        m2.start();
+        m3.start();
+        
     }
     
     public void paint(Graphics g)
@@ -50,6 +56,88 @@ public class JavaLineGame extends JFrame
         g.fillRect(boxBottom.x, boxBottom.y, boxBottom.width, boxBottom.height);
         g.fillRect(boxLeft.x, boxLeft.y, boxLeft.width, boxLeft.height);
         g.fillRect(boxRight.x, boxRight.y, boxRight.width, boxRight.height);
+    }
+    
+    //created the Move1 Thread that will let the user control his box object
+    private class Move1 extends Thread implements KeyListener
+    {
+        
+        public void run()
+        {
+            
+            //add the Keylistener to this Thread
+            addKeyListener(this);
+            
+            //creates an infinite loop.
+            while(true)
+            {
+                repaint();
+                
+                //start a try catch statement to allow the infinite loop to stop if there is an exception.
+                try
+                {
+                    
+                }
+                catch (Exception e)
+                {
+                    //let the loop exit the program if there was an exception thrown.
+                    break;
+                }
+                
+                
+            }
+        }
+        
+        //imported this for KeyListener
+        public void keyPressed(KeyEvent event)
+        {            
+        }
+        
+        //imported this for KeyListener
+        public void keyReleased(KeyEvent event)
+        {
+        }
+        
+        //imported this for KeyListener. Most of the work will be done in this one.
+        public void keyTyped(KeyEvent event)
+        {
+            
+        }
+        
+    }
+    
+    //I created this Thread, but I do not remember what I was Planning on doing with it.
+    private class Move2 extends Thread implements KeyListener
+    {
+        
+        public void run()
+        {
+            
+            addKeyListener(this);
+            
+            while(true)
+            {
+                repaint();
+            }
+        }
+        public void keyPressed(KeyEvent event)
+        {
+        }
+        
+        public void keyReleased(KeyEvent event)
+        {
+        }
+        
+        public void keyTyped(KeyEvent event)
+        {
+            
+        }
+    }
+    
+    //This thread will control the Javelins falling from the sky.
+    private class Move3 extends Thread
+    {
+        
     }
     
     
